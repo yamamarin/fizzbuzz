ee4032d449c82606e2fad3df7dd7ecb9c36197f4
yamamarin <yamazaki@brain.imi.i.u-tokyo.ac.jp>
2014-05-12 19:27:02 +0900

    change fizz to foo, buzz to bar

--- FizzBuzz.c
+++ FizzBuzz.c
@@ -5,9 +5,9 @@ int main()
   int i;
   for(i=1;i<=100;i++){
     if(i%3==0){
-      if(i%5==0) printf("FizzBuzz\n");
-      else printf("Fizz\n");
-    }else if(i%5==0) printf("Buzz\n");
+      if(i%5==0) printf("FooBar\n");
+      else printf("Foo\n");
+    }else if(i%5==0) printf("Bar\n");
     else printf("%d\n",i);
   }
   return 0;
