c60d051c2d963101b2e3d6ee2cd51d25daea27f6
yamamarin <yamazaki@brain.imi.i.u-tokyo.ac.jp>
2014-05-12 19:17:10 +0900

    add solve fizzbuzz func

---
+++ FizzBuzz.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+int main()
+{
+  int i;
+  for(i=1;i<=100;i++){
+    if(i%3==0){
+      if(i%5==0) printf("FizzBuzz\n");
+      else printf("Fizz\n");
+    }else if(i%5==0) printf("Buzz\n");
+    else printf("%d\n",i);
+  }
+  return 0;
+}
